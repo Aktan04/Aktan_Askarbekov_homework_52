e9e5185b14ade88d4040d2d326bd0c44c291c2f8
Askarbekov Aktan <akn392204@gmail.com>
2024-04-15 15:01:46 +0600

    Add new fields in model of Phone. Add new migrations and changed some Views

---
+++ Migrations/20240415090013_AddDescriptionAndAvatar.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoneProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionAndAvatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Phones",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Phones",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Phones");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Phones");
+        }
+    }
+}
--- Models/Phone.cs
+++ Models/Phone.cs
@@ -6,4 +6,6 @@ public class Phone
     public string Name { get; set; }
     public string Company { get; set; }
     public int Price { get; set; }
+    public string? Description { get; set; }
+    public string Avatar { get; set; }
 }
